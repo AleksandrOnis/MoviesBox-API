5b96ae17b4f3fd6bf06115d31c8b6c4fcc225e7f
AleksandrOnis <80990738+AleksandrOnis@users.noreply.github.com>
2022-05-05 13:26:51 +0300

    added authorization

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const cors = require("cors");
 const logger = require("morgan");
 require("dotenv").config();
 
+const { authRouter, usersRouter } = require("./routes/api");
+
 const app = express();
+
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRouter);
+app.use("/api/users", usersRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
---
+++ models/user.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const userSchema = Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: emailRegexp,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    token: {
+      type: String,
+      default: "",
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const signupJoiSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const User = model("user", userSchema);
+
+const schemas = {
+  signup: signupJoiSchema,
+};
+
+module.exports = {
+  User,
+  schemas,
+};
