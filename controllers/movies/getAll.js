ade7ac2d3480d18f0c795038a2dec3c942394596
AleksandrOnis <80990738+AleksandrOnis@users.noreply.github.com>
2022-06-26 13:52:49 +0300

    Update getAll.js
    add sort

--- controllers/movies/getAll.js
+++ controllers/movies/getAll.js
@@ -2,16 +2,16 @@ const { Movie } = require("../../models/movie");
 
 const getAll = async (req, res, next) => {
   const { _id } = req.user;
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 10 } = req.query;
   const skip = (page - 1) * limit;
   try {
     const count = await Movie.find({ owner: _id }).count();
     if (!count) res.json({ result: 0, total_pages: 0 });
     const total_pages = Math.ceil(count / limit);
-    const result = await Movie.find({ owner: _id }, "-createdAt -updatedAt", {
+    const result = await Movie.find({ owner: _id }, "-updatedAt", {
       skip,
       limit: Number(limit),
-    });
+    }).sort("-createdAt");
     res.json({ result, total_pages });
   } catch (error) {
     next(error);
