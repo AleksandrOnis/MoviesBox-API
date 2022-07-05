570fed4667d13152b682316f008b251f283d8013
AleksandrOnis <sashaonis@gmail.com>
2022-07-05 16:53:06 +0300

    add moviesIds

--- controllers/movies/getAll.js
+++ controllers/movies/getAll.js
@@ -5,6 +5,8 @@ const getAll = async (req, res, next) => {
   const { page = 1, limit = 10 } = req.query;
   const skip = (page - 1) * limit;
   try {
+    const movies = await Movie.find({ owner: _id });
+    const moviesIds = movies.map((movie) => movie.movieId);
     const count = await Movie.find({ owner: _id }).count();
     if (!count) res.json({ result: 0, total_pages: 0 });
     const total_pages = Math.ceil(count / limit);
@@ -12,7 +14,7 @@ const getAll = async (req, res, next) => {
       skip,
       limit: Number(limit),
     }).sort("-createdAt");
-    res.json({ result, total_pages });
+    res.json({ result, total_pages, moviesIds });
   } catch (error) {
     next(error);
   }
